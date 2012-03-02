5f9e9015b10edc97aaf7d7688e27aed32af4d971
Bhushan Ahire <bhushan.ahire@synechron.com>
2012-03-02 18:09:01 +0530

    Testing for android.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -13,6 +13,7 @@ ListDemo = new Ext.Application({
             store: ListDemo.ListStore,
             itemTpl: '<div class="contact">{time} : {task} </div>',	    
             grouped: true,
+	    flex: 1,
             onItemDisclosure: function(record, btn, index) {
                 ListDemo.detailPanel.update(record.data);
                 ListDemo.Viewport.setActiveItem('detailpanel');
@@ -29,6 +30,56 @@ ListDemo = new Ext.Application({
             }
 
 */
+
+
+/*
+
+var app =  new Ext.Panel({
+        fullscreen: true,
+        layout: {
+           type: 'vbox',
+           align: 'stretch'
+        },
+        dockedItems:[{
+            xtype: 'toolbar',
+            title: 'Ruby Conf 2012',
+
+        }],
+        items: [
+        {
+            xtype: 'panel',
+            title:'topPanel',
+            height: 50,
+            items:{
+                xtype:'button',
+                cls: 'editChildBtn',
+                text: 'Einstellungen',
+                width: 150,
+                handler: function(){}
+            }
+        },
+        {
+            xtype: 'list',
+            flex: 1,
+            style: 'background-color: Transparent;',
+            id: 'MyList',
+            allowDeselect: true,
+            clearSelectionOnDeactivate: true,
+            //layout: 'fit',
+            store: simfla.ux.plugins.demo.store,
+            itemTpl: '{firstName} <strong>{lastName}</strong>',
+            grouped: false,
+            indexBar: false,
+            singleSelect: true,
+            }]
+        });
+
+
+
+*/
+
+
+
 
 
         ListDemo.Viewport = new Ext.Panel ({
